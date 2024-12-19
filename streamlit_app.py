93f62deb93dea1d78c4a85b985ed63bac09461e0
DevKevRev <Kevin.Cutt@nhs.scot>
2024-12-19 19:01:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #covert the sp dataframe to pd df to use LOC funct
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 6 ingredients:'
